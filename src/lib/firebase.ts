d24cb1c8664c7dd1e64796e62dc039560097a5d0
Steve Hemenway <stevehemenway@gmail.com>
2025-08-16 20:35:22 +0000

    be specific about what you need and where I can reference it to provide

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,10 +1,11 @@
+
 // This configuration is public and can be safely exposed to the client.
 // Firebase security rules are used to protect data.
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "API_KEY",
+  authDomain: "PROJECT_ID.firebaseapp.com",
+  projectId: "PROJECT_ID",
+  storageBucket: "PROJECT_ID.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
 };
